ad498f89e7cb2a1f986a29feb08c9c4b073e8987
Nao-RandD <nao.r.and.d@gmail.com>
2022-08-16 17:16:55 +0700

    [add] アプリで管理するデータをBindingする処理を追加

---
+++ SwiftUI_Tutorials/ThemePicker.swift
@@ -0,0 +1,27 @@
+//
+//  ThemePicker.swift
+//  SwiftUI_Tutorials
+//
+//  Created by Naoyuki Kan on 2022/08/16.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
